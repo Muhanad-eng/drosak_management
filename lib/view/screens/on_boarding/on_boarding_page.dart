162aaafe94c6256e79c25767bd08ee3a5ecd1dd1
Muhanad-eng <dbo.muhanad@gmail.com>
2025-01-15 14:23:04 +0300

    add aa galaxy font and svg flutter package

--- lib/view/screens/on_boarding/on_boarding_page.dart
+++ lib/view/screens/on_boarding/on_boarding_page.dart
@@ -1,13 +1,30 @@
+
+
+import 'package:drosak_management/core/resources/assets_images_manager.dart';
 import 'package:drosak_management/core/resources/color_manager.dart';
+import 'package:drosak_management/core/resources/constants_values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       backgroundColor: ColorManager.kBlack,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         Container(
+          width: double.infinity,
+          child: SvgPicture.asset(AssetsImagesManager.onBoardingImage2)),
+          const SizedBox(height: 50,),
+          const Text(ConstantsValues.konBoardingText1,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
+
+        ],
+      ),
     );
   }
 }
